82be6ca3b0a48d9535602c01057adba55ba5c28a
Михаил Толмачев <mishytka-02@mail.ru>
2021-05-30 20:10:41 +0300

    Добаввил 2 табличные формы к запросам "Список автомобилей" и "Отдел кадров"

--- GIBDD/FormMain.Designer.cs
+++ GIBDD/FormMain.Designer.cs
@@ -50,6 +50,9 @@ namespace yt_DesignUI
             this.AboutProgramm = new yt_DesignUI.yt_Button();
             this.egoldsFormStyle1 = new yt_DesignUI.Components.EgoldsFormStyle(this.components);
             this.roundingButtonsComponent1 = new yt_DesignUI.Components.RoundingButtonsComponent(this.components);
+            this.Cadr_FormMain_Button = new yt_DesignUI.yt_Button();
+            this.ListOfCars_FormMain_Button = new yt_DesignUI.yt_Button();
+            this.ListOfHijackings = new yt_DesignUI.yt_Button();
             this.tabControl1.SuspendLayout();
             this.tabPage1.SuspendLayout();
             this.tabPage2.SuspendLayout();
@@ -468,6 +471,84 @@ namespace yt_DesignUI
             this.roundingButtonsComponent1.Rounding = 60;
             this.roundingButtonsComponent1.RoundingEnable = true;
             this.roundingButtonsComponent1.TargetForm = this;
+            // 
+            // Cadr_FormMain_Button
+            // 
+            this.Cadr_FormMain_Button.BackColor = System.Drawing.Color.RoyalBlue;
+            this.Cadr_FormMain_Button.BackColorAdditional = System.Drawing.Color.Tomato;
+            this.Cadr_FormMain_Button.BackColorGradientEnabled = true;
+            this.Cadr_FormMain_Button.BackColorGradientMode = System.Drawing.Drawing2D.LinearGradientMode.BackwardDiagonal;
+            this.Cadr_FormMain_Button.BorderColor = System.Drawing.Color.RoyalBlue;
+            this.Cadr_FormMain_Button.BorderColorEnabled = true;
+            this.Cadr_FormMain_Button.BorderColorOnHover = System.Drawing.Color.Maroon;
+            this.Cadr_FormMain_Button.BorderColorOnHoverEnabled = true;
+            this.Cadr_FormMain_Button.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.Cadr_FormMain_Button.Font = new System.Drawing.Font("Verdana", 8.25F);
+            this.Cadr_FormMain_Button.ForeColor = System.Drawing.Color.White;
+            this.Cadr_FormMain_Button.Location = new System.Drawing.Point(20, 50);
+            this.Cadr_FormMain_Button.Name = "Cadr_FormMain_Button";
+            this.Cadr_FormMain_Button.RippleColor = System.Drawing.Color.White;
+            this.Cadr_FormMain_Button.RoundingEnable = true;
+            this.Cadr_FormMain_Button.Size = new System.Drawing.Size(131, 33);
+            this.Cadr_FormMain_Button.TabIndex = 21;
+            this.Cadr_FormMain_Button.Text = "Отдел кадров";
+            this.Cadr_FormMain_Button.TextHover = "";
+            this.Cadr_FormMain_Button.UseDownPressEffectOnClick = true;
+            this.Cadr_FormMain_Button.UseRippleEffect = true;
+            this.Cadr_FormMain_Button.UseZoomEffectOnHover = true;
+            this.Cadr_FormMain_Button.Click += new System.EventHandler(this.Cadr_FormMain_Button_Click);
+            // 
+            // ListOfCars_FormMain_Button
+            // 
+            this.ListOfCars_FormMain_Button.BackColor = System.Drawing.Color.RoyalBlue;
+            this.ListOfCars_FormMain_Button.BackColorAdditional = System.Drawing.Color.Tomato;
+            this.ListOfCars_FormMain_Button.BackColorGradientEnabled = true;
+            this.ListOfCars_FormMain_Button.BackColorGradientMode = System.Drawing.Drawing2D.LinearGradientMode.BackwardDiagonal;
+            this.ListOfCars_FormMain_Button.BorderColor = System.Drawing.Color.RoyalBlue;
+            this.ListOfCars_FormMain_Button.BorderColorEnabled = true;
+            this.ListOfCars_FormMain_Button.BorderColorOnHover = System.Drawing.Color.Maroon;
+            this.ListOfCars_FormMain_Button.BorderColorOnHoverEnabled = true;
+            this.ListOfCars_FormMain_Button.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.ListOfCars_FormMain_Button.Font = new System.Drawing.Font("Verdana", 8.25F);
+            this.ListOfCars_FormMain_Button.ForeColor = System.Drawing.Color.White;
+            this.ListOfCars_FormMain_Button.Location = new System.Drawing.Point(252, 50);
+            this.ListOfCars_FormMain_Button.Name = "ListOfCars_FormMain_Button";
+            this.ListOfCars_FormMain_Button.RippleColor = System.Drawing.Color.White;
+            this.ListOfCars_FormMain_Button.RoundingEnable = true;
+            this.ListOfCars_FormMain_Button.Size = new System.Drawing.Size(131, 33);
+            this.ListOfCars_FormMain_Button.TabIndex = 23;
+            this.ListOfCars_FormMain_Button.Text = "Список автомобилей";
+            this.ListOfCars_FormMain_Button.TextHover = "";
+            this.ListOfCars_FormMain_Button.UseDownPressEffectOnClick = true;
+            this.ListOfCars_FormMain_Button.UseRippleEffect = true;
+            this.ListOfCars_FormMain_Button.UseZoomEffectOnHover = true;
+            this.ListOfCars_FormMain_Button.Click += new System.EventHandler(this.ListOfCars_FormMain_Button_Click);
+            // 
+            // ListOfHijackings
+            // 
+            this.ListOfHijackings.BackColor = System.Drawing.Color.RoyalBlue;
+            this.ListOfHijackings.BackColorAdditional = System.Drawing.Color.Tomato;
+            this.ListOfHijackings.BackColorGradientEnabled = true;
+            this.ListOfHijackings.BackColorGradientMode = System.Drawing.Drawing2D.LinearGradientMode.BackwardDiagonal;
+            this.ListOfHijackings.BorderColor = System.Drawing.Color.RoyalBlue;
+            this.ListOfHijackings.BorderColorEnabled = true;
+            this.ListOfHijackings.BorderColorOnHover = System.Drawing.Color.Maroon;
+            this.ListOfHijackings.BorderColorOnHoverEnabled = true;
+            this.ListOfHijackings.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.ListOfHijackings.Font = new System.Drawing.Font("Verdana", 8.25F);
+            this.ListOfHijackings.ForeColor = System.Drawing.Color.White;
+            this.ListOfHijackings.Location = new System.Drawing.Point(139, 138);
+            this.ListOfHijackings.Name = "ListOfHijackings";
+            this.ListOfHijackings.RippleColor = System.Drawing.Color.White;
+            this.ListOfHijackings.RoundingEnable = true;
+            this.ListOfHijackings.Size = new System.Drawing.Size(131, 33);
+            this.ListOfHijackings.TabIndex = 22;
+            this.ListOfHijackings.Text = "Список угонов";
+            this.ListOfHijackings.TextHover = "";
+            this.ListOfHijackings.UseDownPressEffectOnClick = true;
+            this.ListOfHijackings.UseRippleEffect = true;
+            this.ListOfHijackings.UseZoomEffectOnHover = true;
+            this.ListOfHijackings.Click += new System.EventHandler(this.ListOfHijackings_Click);
             // 
             // FormMain
             // 
